3f7463956e18fe5aac01179e7c68e38a6a310e40
Nur Wahid Muhammad <nwahidm@gmail.com>
2024-02-11 16:38:55 +0700

    minor update

--- api/v1/index.js
+++ api/v1/index.js
@@ -7,214 +7,214 @@ const IndexService = require("../../services/index");
 
 const { isVerified } = require("../../middlewares/is_verified");
 
-router.post("/lapdumas", async (req, res) => {
-  const lapdumasData = req.body;
+// router.post("/lapdumas", async (req, res) => {
+//   const lapdumasData = req.body;
 
-  const result = await IndexService.CreateLapdumas(lapdumasData);
-  log("result", result);
+//   const result = await IndexService.CreateLapdumas(lapdumasData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/lapdumas/file", async (req, res) => {
-  const lapdumasData = req.body;
+// router.post("/lapdumas/file", async (req, res) => {
+//   const lapdumasData = req.body;
 
-  const result = await IndexService.CreateLapdumasFiles(lapdumasData);
-  log("result", result);
+//   const result = await IndexService.CreateLapdumasFiles(lapdumasData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/penyelidikan", async (req, res) => {
-  const penyelidikanData = req.body;
+// router.post("/penyelidikan", async (req, res) => {
+//   const penyelidikanData = req.body;
 
-  const result = await IndexService.CreatePenyelidikan(penyelidikanData);
-  log("result", result);
+//   const result = await IndexService.CreatePenyelidikan(penyelidikanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/penyelidikan/file", async (req, res) => {
-  const penyelidikanData = req.body;
+// router.post("/penyelidikan/file", async (req, res) => {
+//   const penyelidikanData = req.body;
 
-  const result = await IndexService.CreatePenyelidikanFiles(penyelidikanData);
-  log("result", result);
+//   const result = await IndexService.CreatePenyelidikanFiles(penyelidikanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/penyelidikan/jaksa", async (req, res) => {
-  const penyelidikanData = req.body;
+// router.post("/penyelidikan/jaksa", async (req, res) => {
+//   const penyelidikanData = req.body;
 
-  const result = await IndexService.CreatePenyelidikanJaksa(penyelidikanData);
-  log("result", result);
+//   const result = await IndexService.CreatePenyelidikanJaksa(penyelidikanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/penyidikan", async (req, res) => {
-  const penyidikanData = req.body;
+// router.post("/penyidikan", async (req, res) => {
+//   const penyidikanData = req.body;
 
-  const result = await IndexService.CreatePenyidikan(penyidikanData);
-  log("result", result);
+//   const result = await IndexService.CreatePenyidikan(penyidikanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/penyidikan/file", async (req, res) => {
-  const penyidikanData = req.body;
+// router.post("/penyidikan/file", async (req, res) => {
+//   const penyidikanData = req.body;
 
-  const result = await IndexService.CreatePenyidikanFiles(penyidikanData);
-  log("result", result);
+//   const result = await IndexService.CreatePenyidikanFiles(penyidikanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/penyidikan/jaksa", async (req, res) => {
-  const penyidikanData = req.body;
+// router.post("/penyidikan/jaksa", async (req, res) => {
+//   const penyidikanData = req.body;
 
-  const result = await IndexService.CreatePenyidikanJaksa(penyidikanData);
-  log("result", result);
+//   const result = await IndexService.CreatePenyidikanJaksa(penyidikanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/prapenuntutan", async (req, res) => {
-  const penuntutanData = req.body;
+// router.post("/prapenuntutan", async (req, res) => {
+//   const penuntutanData = req.body;
 
-  const result = await IndexService.CreatePrapenuntutan(penuntutanData);
-  log("result", result);
+//   const result = await IndexService.CreatePrapenuntutan(penuntutanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/prapenuntutan/file", async (req, res) => {
-  const penuntutanData = req.body;
+// router.post("/prapenuntutan/file", async (req, res) => {
+//   const penuntutanData = req.body;
 
-  const result = await IndexService.CreatePrapenuntutanFiles(penuntutanData);
-  log("result", result);
+//   const result = await IndexService.CreatePrapenuntutanFiles(penuntutanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
-router.post("/prapenuntutan/jaksa", async (req, res) => {
-  const penuntutanData = req.body;
+// router.post("/prapenuntutan/jaksa", async (req, res) => {
+//   const penuntutanData = req.body;
 
-  const result = await IndexService.CreatePrapenuntutanJaksa(penuntutanData);
-  log("result", result);
+//   const result = await IndexService.CreatePrapenuntutanJaksa(penuntutanData);
+//   log("result", result);
 
-  if (result.error) {
-    return res.status(400).json({
-      status: 400,
-      error: result.error,
-    });
-  } else {
-    return res.status(200).json({
-      status: 200,
-      data: result,
-    });
-  }
-});
+//   if (result.error) {
+//     return res.status(400).json({
+//       status: 400,
+//       error: result.error,
+//     });
+//   } else {
+//     return res.status(200).json({
+//       status: 200,
+//       data: result,
+//     });
+//   }
+// });
 
 router.get("/lapdumas", [isVerified], async (req, res) => {
   const user = req.user;
--- middlewares/is_verified.js
+++ middlewares/is_verified.js
@@ -1,22 +1,28 @@
-const jwt = require('jsonwebtoken');
-const debug = require('debug');
-const { split } = require('lodash');
+const jwt = require("jsonwebtoken");
+const debug = require("debug");
+const { split } = require("lodash");
 
 module.exports = {
-    isVerified: async (req, res, next) => {
-        const log = debug('integration-service:is_verified')
-        try {
-            const { sifig_token } = req.headers;
-            const token = split(sifig_token, ' ')[0];
-    
-            if (!token) return res.status(401).json({ message: 'Invalid token! The access token is missing', error: 'invalid_token' })
-            
-            const decoded = jwt.verify(token, config.myConfig.sessionSecret);
-            log('decoded: ', decoded);
-            req.user = decoded;
-            next();
-        } catch (error) {
-            return res.status(401).json({ message: 'Invalid token!', error })
-        }
+  isVerified: async (req, res, next) => {
+    const log = debug("integration-service:is_verified");
+    try {
+      const indexHeaders = req.headers;
+      const token = split(indexHeaders.authorization, " ")[1];
+
+      if (!token)
+        return res
+          .status(401)
+          .json({
+            message: "Invalid token! The access token is missing",
+            error: "invalid_token",
+          });
+
+      const decoded = jwt.verify(token, config.myConfig.sessionSecret);
+      log("decoded: ", decoded);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Invalid token!", error });
     }
+  },
 };
